57cf6a1cde5454a69137272228ccb48d429f24ab
Praneeth T T <praneethgopinathan@gmail.com>
2024-08-27 15:38:34 +0530

    fixed: frontend issue

--- employee_project/employee-list/src/api.js
+++ employee_project/employee-list/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// console.log('API_BASE_URL:', API_BASE_URL);
 
 export const listEmployees = async () => {
   try {
